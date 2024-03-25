373ce12564c42d0be8761800543d02200578949d
TanyaSingh67 <tanyaplatformideas@gmail.com>
2024-03-25 12:20:20 +0530

    modified homepage

--- hackslashtask1/lib/screens/homePage.dart
+++ hackslashtask1/lib/screens/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackslashtask1/utils/global.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -6,7 +7,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:SafeArea(child: SingleChildScrollView(
-        
+       child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: darkbackgroundColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 30)),
+                
+              ],
+            ),
+       ), 
       ))
     );
   }
