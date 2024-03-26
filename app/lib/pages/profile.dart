948942e2660a9f85a28c90838ce39c344e607ff8
divv21 <divijasharma21@gmail.com>
2024-03-26 19:38:06 +0530

    fixed profile page

--- app/lib/pages/profile.dart
+++ app/lib/pages/profile.dart
@@ -24,71 +24,95 @@ class ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold (bottomNavigationBar: NavBar(),
-            appBar: AppBar(
-              backgroundColor: Color.fromRGBO(0, 13, 29, 0),
-              title: Text('Profile',
-              style: TextStyle(
-                  color: Colors.grey
-              ),),
-            ),
-            body: SizedBox(
-              height: 700,
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
-                child: Column(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 27, 59, 1),
+        centerTitle: true,
+        title: Text('Profile',
+          style: TextStyle(
+              color: Colors.grey
+          ),),
+        leading: Icon(Icons.arrow_back_ios,
+          color: Color.fromRGBO(5, 96, 250, 1),),
+      ),
+      body: SizedBox(
+        height: 700,
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 48.0),
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Spacer(),
-                    Container(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                            backgroundImage: AssetImage("assets/images/pfp.png"),
-                          ),
-                          Column(children: [
-                            Text('Ken Nwaeze'),
-                            Row(
-                              children: [
-                                Text('Current balance:'),
-                                Text('N10,712:00',
-                                style: TextStyle(
+                    CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/pfp.png"),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Ken Nwaeze',
+                          style: TextStyle(
+                              color: Colors.white
+                          ),),
+                        Row(
+                          children: [
+                            Text('Current balance:',
+                              style: TextStyle(
+                                  color: Colors.white
+                              ),),
+                            Text('  N10,712:00',
+                              style: TextStyle(
                                   color: Color.fromRGBO(5, 96, 250, 1)
-                                ),)
-                              ],
-                            )
-                          ],)
-                        ],
-                      ),
-                    ),
-                    Spacer(),
-                    Container(
-
-
-                    ),
-                    Spacer(),
-                    Expanded(
-                      child: ListView.builder(
-                          itemCount: settings.length,
-                          itemBuilder: (context, index) {
-                          return Card(
-                              child: ListTile(
-                                tileColor: Color.fromRGBO(0, 27, 59, 1),
-                                textColor: Colors.white,
-                                leading: FlutterLogo(),
-                                title: Text(settings[index].settingsname,
-                                ),
-                                subtitle: Text(settings[index].description),
-                                trailing: Icon(Icons.arrow_forward_ios_rounded),
-                              ),
-                            );
-
-                          }
-                      ),
-                    ),
+                              ),)
+                          ],
+                        )
+                      ],)
                   ],
                 ),
               ),
-            ),
+              Container(
+                child: Row(
+                  children: [
+                    Text('Enable dark Mode',
+                      style: TextStyle(
+                          color: Colors.white
+                      ),)
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: settings.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          tileColor: Color.fromRGBO(0, 27, 59, 1),
+                          textColor: Colors.white,
+                          leading: FlutterLogo(),
+                          title: Text(settings[index].settingsname,
+                            style: TextStyle(
+                                fontSize: 16
+                            ),
+                          ),
+                          subtitle: Text(settings[index].description,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),),
+                          trailing: Icon(Icons.arrow_forward_ios_rounded),
+                        ),
+                      );
+
+                    }
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
 
 
 
