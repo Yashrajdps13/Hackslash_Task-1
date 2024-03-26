f6b07d27f45dcbe682799e24119451784829538a
divv21 <divijasharma21@gmail.com>
2024-03-26 18:37:09 +0530

    fixed track page code

--- app/lib/pages/track.dart
+++ app/lib/pages/track.dart
@@ -14,112 +14,169 @@ class _TrackState extends State<Track> {
     return Material(
         child: Scaffold(
       bottomNavigationBar: NavBar(),
-          body: Column(
-            children: [
-              Image.asset("assets/map.png",
-              scale: 30,
-              alignment: Alignment.topCenter),
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
-           child: SingleChildScrollView(
-             child: Column(
-               children: [
-                 Text("Tracking Number",
-                 style: TextStyle(
-                   color: Colors.white
-                 ),),
-                 Text("R-7458-4567-4434-5854",
-                 style: TextStyle(
-                   color: Color.fromRGBO(5, 96, 250,0)
-                 ),),
-                 Text("Package Status",
-                 style: TextStyle(
-                   color: Colors.grey
-                 ),),
-                 CheckboxListTile(
-                   title: Text("Courier requested",
-                   style: TextStyle(
-                     color: Colors.grey
-                   ),),
-                   value: true,
-                   onChanged: (newValue) {},
-                   controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
-                 ),
-                 Text("July 7 2022 08:00 am",
-                 style: TextStyle(
-                   color: Color.fromRGBO(236, 128, 0, 1)
-                 ),),
-                 CheckboxListTile(
-                   title: Text("Package reaady for delivery",
-                     style: TextStyle(
-                         color: Colors.grey
-                     ),),
-                   value: true,
-                   onChanged: (newValue) {},
-                   controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
-                 ),
-                 Text("July 7 2022 08:30 am",
-                   style: TextStyle(
-                       color: Color.fromRGBO(236, 128, 0, 1)
-                   ),),
-                 CheckboxListTile(
-                   title: Text("Package in transit",
-                   style: TextStyle(
-                     color: Color.fromRGBO(5, 96, 250, 1)
-                   ),),
-                   value: false,
-                   onChanged: (newValue) {},
-                   controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
-                 ),
-                 Text("July 7 2022 10:30 am",
-                   style: TextStyle(
-                       color: Color.fromRGBO(236, 128, 0, 1)
-                   ),),
-                 CheckboxListTile(
-                   title: Text("Package delivered",
-                     style: TextStyle(
-                         color: Colors.grey
-                     ),),
-                   value: false,
-                   onChanged: (newValue) {},
-                   controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
-                 ),
-                 Text("July 7 2022 10:30 am",
-                   style: TextStyle(
-                       color: Colors.grey
-                   ),),
-             ElevatedButton(
-               style: ElevatedButton.styleFrom(
-                 backgroundColor: Color.fromRGBO(5, 96, 250, 1),
-                 elevation: 0,
-               ),
-                 onPressed: () {},
-                 child: Text( 'View Package Info',
-                 style: TextStyle(
-                   color: Colors.white
-                 ),)
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 320,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/map.png"),
 
-             )
-               ]
+                    ),),),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
+                  child: SizedBox(
+                    height: 390,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Tracking Number",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16
+                            ),),
+                          Spacer(),
+                          Text("R-7458-4567-4434-5854",
+                            style: TextStyle(
+                                color: Color.fromRGBO(5, 96, 250,1),
+                                fontSize: 16
+                            ),),
+                          Spacer(),
+                          Text("Package Status",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14
+                            ),),
+                          CheckboxListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Courier requested",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14
+                                  ),),
+                                Text("July 7 2022 08:00 am",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(236, 128, 0, 1),
+                                      fontSize: 14
+                                  ),),
+                              ],
+                            ),
+                            value: true,
+                            onChanged: (newValue) {},
+                            controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                          ),
+
+                          CheckboxListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Package reaady for delivery",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14
+                                  ),),
+                                Text("July 7 2022 08:30 am",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(236, 128, 0, 1),
+                                      fontSize: 14
+                                  ),),
+                              ],
+                            ),
+                            value: true,
+                            onChanged: (newValue) {},
+                            controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                          ),
+
+                          CheckboxListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Package in transit",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(5, 96, 250, 1),
+                                      fontSize: 16
+                                  ),),
+                                Text("July 7 2022 10:30 am",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(236, 128, 0, 1),
+                                      fontSize: 14
+                                  ),),
+                              ],
+                            ),
+                            value: false,
+                            onChanged: (newValue) {},
+                            controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                          ),
+
+                          CheckboxListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Package delivered",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14
+                                  ),),
+                                Text("July 7 2022 10:30 am",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14
+                                  ),),
+                              ],
+                            ),
+                            value: false,
+                            onChanged: (newValue) {},
+                            controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
+                          ),
+
+                          SizedBox(
+                            height: 30,
+                          ),
+                          SizedBox(
+                            width: 341,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color.fromRGBO(5, 96, 250, 1),
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5.0)
+                                    )
+
+                                ),
+                                onPressed: () {},
+                                child: Text( 'View Package Info',
+                                    style: TextStyle(
+                                        color: Colors.white
+                                    ),
+                                    textAlign: TextAlign.center)
+
+                            ),
+                          )
+                        ]
 
 
 
-             ),
-           ),         )
-            ],
+                    ),
+                  ),         )
+              ],
 
             ),
-
-
           ),
 
 
+        ),
 
 
 
 
 
-       );
+
+
+    );
 
 
   }
